9d3fe68ad28f4d11d1da1049814ed9480bbbcb81
aminuldit <aminuldit@gmail.com>
2022-08-07 21:56:08 +0600

    contact section start

--- components/contact/Contact.jsx
+++ components/contact/Contact.jsx
@@ -2,7 +2,20 @@
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact' className="contact section_gap">
+        <div className="container contact_container">
+            <div className="title">
+                <h5>Feel Free to</h5>
+                <h2>Contact</h2>
+            </div>
+            <div className="row">
+                <div className="col">
+                    
+                </div>
+                <div className="col"></div>
+            </div>
+        </div>
+    </section>
   )
 }
 
