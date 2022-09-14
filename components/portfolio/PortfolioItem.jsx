ddc213d7e5c47023099c65d3100c05982fccc71f
aminulds <aminulds61@gmail.com>
2022-09-14 09:30:14 +0600

    className issue solve

--- components/portfolio/PortfolioItem.jsx
+++ components/portfolio/PortfolioItem.jsx
@@ -6,29 +6,10 @@ import TechIcon from './TechIcon';
 const PortfolioItem = ({ img, title, technology, github, live }) => {
     return (
         <div className="col">
-            {/* <div className="card item">
+            <div className="card item">
                 <Image src={img} className="card-img-top" height={250} width={500} alt={title} />
-                <div className="item_img">
-                </div>
-                <h5>{title}</h5>
-                <p>
-                    {technology.map((tech) => {
-                        return <TechIcon key={tech.id} icon={tech.icon} />
-                    })}
-                </p>
-                <div className="buttons">
-                    <Link href={github}>
-                        <a className='btn btn_primary' target="_blank">Github</a>
-                    </Link>
-                    <Link href={live}>
-                        <a className='btn' target="_blank">Live Demo</a>
-                    </Link>
-                </div>
-            </div> */}
-            <div class="card item">
-                <Image src={img} class="card-img-top" height={250} width={500} alt={title} />
                 <div class="card-body">
-                    <h5 class="card-title">{title}</h5>
+                    <h5 className="card-title">{title}</h5>
                     <p>
                         {technology.map((tech) => {
                             return <TechIcon key={tech.id} icon={tech.icon} />
