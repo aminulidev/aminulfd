475fff196463787f40e48b69a60e4c8686c818c4
aminulds <aminulds61@gmail.com>
2022-10-31 00:36:28 +0600

    portfolio data update

--- components/portfolio/Portfolio.jsx
+++ components/portfolio/Portfolio.jsx
@@ -10,9 +10,9 @@ const Portfolio = () => {
                     <h5>My Recent Work</h5>
                     <h2>Portfolio</h2>
                 </div>
-                <div className="row g-4 g-md-5 row-cols-1 row-cols-md-2 row-cols-lg-3">
+                <div className="row g-4 row-cols-1 row-cols-md-2 row-cols-lg-3">
                     {
-                        portfolioData.reverse().map((data) => {
+                        portfolioData.map((data) => {
                             return <PortfolioItem key={data.id} img={data.img} title={data.title} technology={data.technology} github={data.github} live={data.live} />
                         })
                     }
--- components/portfolio/data.js
+++ components/portfolio/data.js
@@ -1,12 +1,13 @@
 // Icon
-import { FaHtml5, FaCss3Alt, FaBootstrap } from 'react-icons/fa';
+import { FaHtml5, FaCss3Alt, FaBootstrap, FaReact, FaChartArea, FaSass } from 'react-icons/fa';
 import { IoLogoJavascript } from 'react-icons/io';
+import { SiMaterialui } from 'react-icons/si';
 
 export const portfolioData = [
     {
         id: 1,
         img: "https://www.thethemely.com/images/product_feature_images/070822062021_62efae05d8a8f.jpg",
-        title: "TripHub | Tour and Travel Agency HTML Landing Page",
+        title: "Travel Agency Landing Page",
         technology: [{ id: 1, icon: <FaHtml5 className='icon html5' /> }, { id: 2, icon: <FaCss3Alt className='icon css3' /> }, { id: 3, icon: <FaBootstrap className='icon bootstrap' /> }, { id: 4, icon: <IoLogoJavascript className='icon js' /> }],
 
         github: "#",
@@ -15,7 +16,7 @@ export const portfolioData = [
     {
         id: 2,
         img: "https://www.thethemely.com/images/product_feature_images/020822045058_62e90192c9063.jpg",
-        title: "SaaStemp | SaaS & Software Website Template",
+        title: "SAAS Website Template",
         technology: [{ id: 1, icon: <FaHtml5 className='icon html5' /> }, { id: 2, icon: <FaCss3Alt className='icon css3' /> }, { id: 3, icon: <FaBootstrap className='icon bootstrap' /> }, { id: 4, icon: <IoLogoJavascript className='icon js' /> }],
 
         github: "#",
@@ -24,7 +25,7 @@ export const portfolioData = [
     {
         id: 3,
         img: "https://www.thethemely.com/images/product_feature_images/250122111808_61ef8810078cd.jpg",
-        title: "e-Shop – Mega Shop eCommerce HTML Template",
+        title: "E-Commerce HTML Template",
         technology: [{ id: 1, icon: <FaHtml5 className='icon html5' /> }, { id: 2, icon: <FaCss3Alt className='icon css3' /> }, { id: 3, icon: <FaBootstrap className='icon bootstrap' /> }, { id: 4, icon: <IoLogoJavascript className='icon js' /> }],
 
         github: "#",
@@ -34,7 +35,7 @@ export const portfolioData = [
         id: 4,
         img: "https://i.ibb.co/q7Bpqzm/reactadmindashboard.jpg",
         title: "React Admin Dashboard",
-        technology: [{ id: 1, icon: <FaHtml5 className='icon html5' /> }, { id: 2, icon: <FaCss3Alt className='icon css3' /> }, { id: 3, icon: <FaBootstrap className='icon bootstrap' /> }, { id: 4, icon: <IoLogoJavascript className='icon js' /> }],
+        technology: [{ id: 1, icon: <FaReact className='icon react' /> }, { id: 2, icon: <SiMaterialui className='icon mui' /> }, { id: 3, icon: <FaChartArea className='icon rechart' /> }, { id: 4, icon: <FaSass className='icon sass' /> },],
 
         github: "https://github.com/aminulds/admindashboard",
         live: "https://reactfadmindashboard.netlify.app/"
@@ -50,4 +51,4 @@ export const portfolioData = [
     // },
 
 
-];
+].reverse();
