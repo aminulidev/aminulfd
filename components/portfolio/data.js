21932cb249eeee65c32a99edc8db6dca8f21e08b
aminulds <aminulds61@gmail.com>
2022-10-31 23:20:11 +0600

    icon change

--- components/portfolio/data.js
+++ components/portfolio/data.js
@@ -1,5 +1,5 @@
 // Icon
-import { FaHtml5, FaCss3Alt, FaBootstrap, FaReact, FaChartArea, FaSass } from 'react-icons/fa';
+import { FaHtml5, FaSass, FaBootstrap, FaReact, FaChartArea } from 'react-icons/fa';
 import { IoLogoJavascript } from 'react-icons/io';
 import { SiMaterialui, SiSwiper } from 'react-icons/si';
 import { TbApi } from 'react-icons/tb';
@@ -9,7 +9,7 @@ export const portfolioData = [
         id: 1,
         img: "https://www.thethemely.com/images/product_feature_images/070822062021_62efae05d8a8f.jpg",
         title: "Travel Agency Landing Page",
-        technology: [{ id: 1, icon: <FaHtml5 className='icon html5' /> }, { id: 2, icon: <FaCss3Alt className='icon css3' /> }, { id: 3, icon: <FaBootstrap className='icon bootstrap' /> }, { id: 4, icon: <IoLogoJavascript className='icon js' /> }],
+        technology: [{ id: 1, icon: <FaHtml5 className='icon html5' /> }, { id: 2, icon: <FaSass className='icon sass' /> }, { id: 3, icon: <FaBootstrap className='icon bootstrap' /> }, { id: 4, icon: <IoLogoJavascript className='icon js' /> }],
 
         github: "#",
         live: "https://www.thethemely.com/item/triphub-tour-and-travel-agency-html-landing-page/34"
@@ -18,7 +18,7 @@ export const portfolioData = [
         id: 2,
         img: "https://www.thethemely.com/images/product_feature_images/020822045058_62e90192c9063.jpg",
         title: "SAAS Website Template",
-        technology: [{ id: 1, icon: <FaHtml5 className='icon html5' /> }, { id: 2, icon: <FaCss3Alt className='icon css3' /> }, { id: 3, icon: <FaBootstrap className='icon bootstrap' /> }, { id: 4, icon: <IoLogoJavascript className='icon js' /> }],
+        technology: [{ id: 1, icon: <FaHtml5 className='icon html5' /> }, { id: 2, icon: <FaSass className='icon sass' /> }, { id: 3, icon: <FaBootstrap className='icon bootstrap' /> }, { id: 4, icon: <IoLogoJavascript className='icon js' /> }],
 
         github: "#",
         live: "https://www.thethemely.com/item/saastemp-saas-software-website-template/35"
@@ -27,7 +27,7 @@ export const portfolioData = [
         id: 3,
         img: "https://www.thethemely.com/images/product_feature_images/250122111808_61ef8810078cd.jpg",
         title: "E-Commerce HTML Template",
-        technology: [{ id: 1, icon: <FaHtml5 className='icon html5' /> }, { id: 2, icon: <FaCss3Alt className='icon css3' /> }, { id: 3, icon: <FaBootstrap className='icon bootstrap' /> }, { id: 4, icon: <IoLogoJavascript className='icon js' /> }],
+        technology: [{ id: 1, icon: <FaHtml5 className='icon html5' /> }, { id: 2, icon: <FaSass className='icon sass' /> }, { id: 3, icon: <FaBootstrap className='icon bootstrap' /> }, { id: 4, icon: <IoLogoJavascript className='icon js' /> }],
 
         github: "#",
         live: "https://www.thethemely.com/item/saastemp-saas-software-website-template/35"
@@ -54,7 +54,7 @@ export const portfolioData = [
     //     id: 3,
     //     img: "",
     //     title: "",
-    //     technology: [{ id: 1, icon: <FaHtml5 className='icon html5' /> }, { id: 2, icon: <FaCss3Alt className='icon css3' /> }, { id: 3, icon: <FaBootstrap className='icon bootstrap' /> }, { id: 4, icon: <IoLogoJavascript className='icon js' /> }],
+    //     technology: [{ id: 1, icon: <FaHtml5 className='icon html5' /> }, { id: 2, icon: <FaSass className='icon sass' /> }, { id: 3, icon: <FaBootstrap className='icon bootstrap' /> }, { id: 4, icon: <IoLogoJavascript className='icon js' /> }],
 
     //     github: "#",
     //     live: "#"
